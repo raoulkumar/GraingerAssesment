df3d441b56c1ef33ae7b1151ce7d577f63f38258
raoulkumar <raoulk@umich.edu>
2019-08-05 14:30:57 -0500

    Parsed JSON data and outputs probability for a row of corrupted data
    Functional with one row of data

--- grainger_server.py
+++ grainger_server.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 from io import BytesIO
+import json
+
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -30,15 +32,29 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         #ypredict = LR_pipe.predict_proba(xtest)
         #return second index of ypredict - probability vehicle is top25
         content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
+        body = json.loads(self.rfile.read(content_length))
+        xtest = np.array([float(body['Issue time']), float(body['Plate Expiry Date']), float(body['Agency']), float(body['Fine amount']), float(body['Latitude']), float(body['Longitude'])])
+        xtest = xtest.reshape(1,-1)
+        ypredict = LR_pipe.predict_proba(xtest)
+        yprob = str(ypredict[:,1])
+        print('Probabilty of top 25 make:', ypredict[:,1])
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
+        response.write(b'\n\nData Received ')
+        response.write(b'Probability of Top 25 make:')
+        response.write(str.encode(yprob))
+        response.write(b'\n\n')
         self.wfile.write(response.getvalue())
 
 
 httpd = HTTPServer(('localhost', port), SimpleHTTPRequestHandler)
 httpd.serve_forever()
+
+
+
+'''
+Test json dict
+{'Issue time':'1251.0', 'Plate Expiry Date':'200304.0', 'Agency':'1.0', 'Fine amount':'50.0', 'Latitude':'99999.0', 'Longitude':'99999.0'}
+
+'''
